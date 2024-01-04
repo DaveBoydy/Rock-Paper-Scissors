b3db8c6f3e5a8c832373f60d0a313bec30f329c5
David Boyd <dboyd2005@msn.com>
2024-01-04 18:08:28 +0000

    Added RPS UI
    Refactored to replace user prompts and some log messages
    with a User Interface using HTMl buttons, Event listeners
    and, DOM methods.

--- index.js
+++ index.js
@@ -6,124 +6,166 @@ const SCISSORS = "scissors";
 
 let playerScore = 0;
 let computerScore = 0;
+let gamesTied = 0;
+let matchResult = "undecided";
 let playerChoice;
 let computerChoice;
 
 function getComputerChoice() {
+  let hand = Math.floor(Math.random() * (MAX - MIN + 1)) + MIN;
 
-    let hand = Math.floor(Math.random() * (MAX - MIN + 1)) + MIN;
-
-    switch (hand) {
-        case 1:
-            return ROCK;
-        case 2:
-            return PAPER;
-        case 3:
-            return SCISSORS;
-    }
-
+  switch (hand) {
+    case 1:
+      return ROCK;
+    case 2:
+      return PAPER;
+    case 3:
+      return SCISSORS;
+  }
 }
 
 function playRound(playerSelection, computerSelection) {
+  if (playerSelection == ROCK && computerSelection == ROCK) {
+    gamesTied += 1;
+    youTie();
+    return;
+  }
+  if (playerSelection == PAPER && computerSelection == PAPER) {
+    gamesTied += 1;
+    youTie();
+    return;
+  }
+  if (playerSelection == SCISSORS && computerSelection == SCISSORS) {
+    gamesTied += 1;
+    youTie();
+    return;
+  }
 
-    playerSelection = playerSelection.toLowerCase();
+  if (playerSelection == ROCK && computerSelection == PAPER) {
+    computerScore += 1;
+    youLose();
+    return;
+  }
+  if (playerSelection == PAPER && computerSelection == SCISSORS) {
+    computerScore += 1;
+    youLose();
+    return;
+  }
+  if (playerSelection == SCISSORS && computerSelection == ROCK) {
+    computerScore += 1;
+    youLose();
+    return;
+  }
 
-    if (!(playerSelection === ROCK || playerSelection === PAPER || playerSelection === SCISSORS)) {
-
-        logError();
-        return;
-    }
-
-    if (playerSelection == ROCK && computerSelection == ROCK) {
-
-        logResult("It's a tie, try again");
-        replayRound();
-        return;
-    }
-    if (playerSelection == PAPER && computerSelection == PAPER) {
-
-        logResult("It's a tie, try again");
-        replayRound();
-        return;
-    }
-    if (playerSelection == SCISSORS && computerSelection == SCISSORS) {
-
-        logResult("It's a tie, try again");
-        replayRound();
-        return;
-    }
-
-    if (playerSelection == ROCK && computerSelection == PAPER) {
-
-        computerScore += 1;
-        return logResult("You Lose! Paper beats Rock");
-    }
-    if (playerSelection == PAPER && computerSelection == SCISSORS) {
-
-        computerScore += 1;
-        return logResult("You Lose! Scissors beats Paper");
-    }
-    if (playerSelection == SCISSORS && computerSelection == ROCK) {
-
-        computerScore += 1;
-        return logResult("You Lose! Rock beats Scissors");
-    }
-
-    if (playerSelection == ROCK && computerSelection == SCISSORS) {
-
-        playerScore += 1;
-        return logResult("You Win! Rock beats Scissors");
-    }
-    if (playerSelection == PAPER && computerSelection == ROCK) {
-
-        playerScore += 1;
-        return logResult("You Win! Paper beats Rock");
-    }
-    if (playerSelection == SCISSORS && computerSelection == PAPER) {
-
-        playerScore += 1;
-        return logResult("You Win! Scissors beats Paper");
-    }
+  if (playerSelection == ROCK && computerSelection == SCISSORS) {
+    playerScore += 1;
+    youWin();
+    return;
+  }
+  if (playerSelection == PAPER && computerSelection == ROCK) {
+    playerScore += 1;
+    youWin();
+    return;
+  }
+  if (playerSelection == SCISSORS && computerSelection == PAPER) {
+    playerScore += 1;
+    youWin();
+    return;
+  }
 }
 
 function logResult(result) {
-
-    console.log(result);
+  console.log(result);
 }
 
-function logError() {
+function playGame(event) {
+  let target = event.target;
 
-    console.log("Please enter Rock, Paper or Scissors and try again");
-    replayRound();
+  switch (target.id) {
+    case ROCK:
+      playRound(ROCK, getComputerChoice());
+      break;
+    case PAPER:
+      playRound(PAPER, getComputerChoice());
+      break;
+    case SCISSORS:
+      playRound(SCISSORS, getComputerChoice());
+      break;
+  }
+  displayResults();
 }
 
-function replayRound() {
+let menu = document.querySelector("#menu");
+menu.addEventListener("click", playGame);
 
-    computerChoice = getComputerChoice();
-    playerChoice = prompt("Rock, Paper or Scissors?", "Rock");
-
-    playRound(playerChoice, computerChoice);
+function youLose() {
+  logResult("You Lose! Paper beats Rock");
 }
 
-function game() {
+function youWin() {
+  logResult("You Win! Rock beats Scissors");
+}
 
-    for (let i = 0; i < 5; i++) {
+function youTie() {
+  logResult("It's a tie, try again");
+}
 
-        computerChoice = getComputerChoice();
-        playerChoice = prompt("Rock, Paper or Scissors?", "Rock");
-        playRound(playerChoice, computerChoice);
+function resetRound() {
+  playerScore = 0;
+  computerScore = 0;
+  gamesTied = 0;
+  matchResult = "undecided";
+  player.textContent = `Player score: ${playerScore}`;
+  computer.textContent = `Computer score: ${computerScore}`;
+  tie.textContent = `Tie(s): ${gamesTied}`;
+  winner.textContent = `Winner: ${matchResult}`;
+}
 
-        console.log("Player Score: " + playerScore);
-        console.log("Computer Score: " + computerScore);
-    }
+function displayResults() {
+  let roundsPlayed = playerScore + computerScore;
 
+  console.log(roundsPlayed);
+
+  if (roundsPlayed == 5) {
+    console.log("Game Over");
     if (playerScore > computerScore) {
-
-        console.log("You won! congratulations you beat the computer.")
+      console.log("Player won best of 5!");
+      matchResult = "Player wins this round!";
+      winner.textContent = `Winner: ${matchResult}`;
     } else {
-        console.log("you lost! the computer beat you.")
+      console.log("compter won best of 5!");
+      matchResult = "Computer wins this round!";
+      winner.textContent = `Winner: ${matchResult}`;
     }
 
+    console.log("resetting game...");
+    setTimeout(resetRound, "3000");
+  }
+  console.log("Player Score: " + playerScore);
+  console.log("Computer Score: " + computerScore);
+
+  player.textContent = `Player score: ${playerScore}`;
+  computer.textContent = `Computer score: ${computerScore}`;
+  tie.textContent = `Tie: ${gamesTied}`;
 }
 
-game();
+const results = document.querySelector("#results");
+results.style.border = "2px solid black";
+results.style.margin = "8px 0";
+results.style.padding = "8px";
+
+const player = document.createElement("p");
+player.textContent = `Player score: ${playerScore}`;
+results.appendChild(player);
+
+const computer = document.createElement("p");
+computer.textContent = `Computer score: ${computerScore}`;
+results.appendChild(computer);
+
+const tie = document.createElement("p");
+tie.textContent = `Tie(s): ${gamesTied}`;
+results.appendChild(tie);
+
+const winner = document.createElement("p");
+winner.textContent = `Winner: ${matchResult}`;
+results.appendChild(winner);
